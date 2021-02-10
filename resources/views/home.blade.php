4abfc93dcd4401ee59cb0a29f7c8cb3ca8fc7d60
Federico <federico.scarpati1989@gmail.com>
2021-02-10 16:49:28 +0100

    Stile in html
    Un po' di stile in html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,77 @@
 @include('template.head')
 
-<body>
-    <header>
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+        .full-height {
+            height: 100vh;
+        }
 
-    </header>
-    <main>
-        <a href="about">About</a>
-    </main>
-    <footer>
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 90px;
+        }
+
+        .links > a {
+            color: #1E90FF;
+            padding: 0 25px;
+            font-size: 30px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+        h3 {
+            font-size: 100px;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+    <body>
+        <header>
+
+        </header>
+        <main>
+           <div class="flex-center position-ref full-height">
+             <div class="content">
+                 <div class="title m-b-md">
+                     <h3>Prima pagina</h3>
+                  </div>
+                   <div class="links">
+                       <a href="about">About</a>
+                   </div>
+             </div>
+         </div>
+        </main>
+        <footer>
     
-    </footer>
-</body>
+        </footer>
+    </body>
 </html>
